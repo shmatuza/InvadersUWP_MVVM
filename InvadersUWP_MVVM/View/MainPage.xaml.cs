7c2e06ed29da4ee082d6ece9ede7e09eabd840ac
Damian Matusek <shmatuza@hotmail.com>
2017-05-08 21:01:05 +0200

    working on Model

--- InvadersUWP_MVVM/View/MainPage.xaml.cs
+++ InvadersUWP_MVVM/View/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -29,27 +30,78 @@ namespace InvadersUWP_MVVM
 
         private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-
+            UpdatePlayAreaSize(new Size(e.NewSize.Width, e.NewSize.Height - 160));
         }
 
         private void Page_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
         {
-
+            if (e.Delta.Translation.X < -1)
+                viewModel.LeftGestureStarted();
+            else if (e.Delta.Translation.X > 1)
+                viewModel.RightGestureStarted();
         }
 
         private void Page_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
         {
-
+            viewModel.LeftGestureCompleted();
+            viewModel.RightGestureCompleted();
         }
 
         private void Page_Tapped(object sender, TappedRoutedEventArgs e)
         {
-
+            viewModel.Tapped();
         }
 
         private void playArea_Loaded(object sender, RoutedEventArgs e)
         {
+            UpdatePlayAreaSize(playArea.RenderSize);
+        }
 
+        private void UpdatePlayAreaSize(Size newPlayAreaSize)
+        {
+            double targetWidth;
+            double targetHeight;
+            if(newPlayAreaSize.Width > newPlayAreaSize.Height)
+            {
+                targetWidth = newPlayAreaSize.Height * 4 / 3;
+                targetHeight = newPlayAreaSize.Height;
+                double leftRightMargin = (newPlayAreaSize.Width - targetWidth) / 2;
+                playArea.Margin = new Thickness(leftRightMargin, 0, leftRightMargin, 0);
+            }
+            else
+            {
+                targetHeight = newPlayAreaSize.Width * 3 / 4;
+                targetWidth = newPlayAreaSize.Width;
+                double topBottomMargin = (newPlayAreaSize.Height - targetHeight) / 2;
+                playArea.Margin = new Thickness(0, topBottomMargin, 0, topBottomMargin);
+            }
+            playArea.Width = targetWidth;
+            playArea.Height = targetHeight;
+            viewModel.PlayAreaSize = playArea.RenderSize;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Window.Current.CoreWindow.KeyDown += KeyDownHandler;
+            Window.Current.CoreWindow.KeyUp += KeyUpHandler;
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            Window.Current.CoreWindow.KeyDown -= KeyDownHandler;
+            Window.Current.CoreWindow.KeyUp -= KeyUpHandler;
+            base.OnNavigatedFrom(e);
+        }
+
+        private void KeyDownHandler(object sender, KeyEventArgs e)
+        {
+            viewModel.KeyDown(e.VirtualKey);
+        }
+
+        private void KeyUpHandler(object sender, KeyEventArgs e)
+        {
+            viewModel.KeyUp(e.VirtualKey);
         }
     }
 }
