3eac993bdd869cfc905e7e249d479801eb712b09
Damian Matusek <shmatuza@hotmail.com>
2017-05-09 14:46:14 +0200

    ViewModel helper

--- InvadersUWP_MVVM/View/InvadersHelper.cs
+++ InvadersUWP_MVVM/View/InvadersHelper.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Foundation;
 using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Shapes;
 
 namespace InvadersUWP_MVVM.View
 {
     class InvadersHelper
     {
-        public static void StarFactory(int random)
+        private readonly Random _random = new Random();
+        public static FrameworkElement StarFactory(Point point, double scale) //TODO
         {
-            
+            FrameworkElement star;
+            switch(_random.Next(3))
+            {
+
+            }
         }
 
         private Color RandomStarColor()
@@ -24,9 +34,26 @@ namespace InvadersUWP_MVVM.View
             return color;
         }
 
+        public static FrameworkElement ScanLineFactory(int y, int width, double scale)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Fill = new SolidColorBrush(Colors.White);
+            rectangle.Height = 2;
+            rectangle.Opacity = .1;
+            rectangle.Width = width * scale;
+            SetCanvasLocation(rectangle, 0, y * scale);
+            return rectangle;
+        }
+
         internal static BitmapImage CreateImageFromAssets(string imageFilename)
         {
             return new BitmapImage(new Uri("ms-appx:///Assets/" + imageFilename));
+        }
+
+        public static void SetCanvasLocation(UIElement control, double x, double y)
+        {
+            Canvas.SetLeft(control, x);
+            Canvas.SetTop(control, y);
         }
     }
 }
