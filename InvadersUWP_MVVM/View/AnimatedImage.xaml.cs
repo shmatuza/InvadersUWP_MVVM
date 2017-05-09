800cf7e3b60c5602b79203f0eaaffd92303543ab
Damian Matusek <shmatuza@hotmail.com>
2017-05-09 14:05:00 +0200

    working on View

---
+++ InvadersUWP_MVVM/View/AnimatedImage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace InvadersUWP_MVVM.View
+{
+    public sealed partial class AnimatedImage : UserControl
+    {
+        public AnimatedImage()
+        {
+            this.InitializeComponent();
+        }
+
+        public void InvaderShot()
+        {
+            invaderShotStoryboard.Begin();
+        }
+
+        public void StartFlashing()
+        {
+            flashStoryboard.Begin();
+        }
+
+        public void StopFlashing()
+        {
+            flashStoryboard.Stop();
+        }
+    }
+}
