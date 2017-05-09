00aeb77ae625b3838a7d537fb2d1606430761ae5
Damian Matusek <shmatuza@hotmail.com>
2017-05-09 14:35:31 +0200

    working on View

--- InvadersUWP_MVVM/View/AnimatedImage.xaml.cs
+++ InvadersUWP_MVVM/View/AnimatedImage.xaml.cs
@@ -26,6 +26,35 @@ namespace InvadersUWP_MVVM.View
             this.InitializeComponent();
         }
 
+        public AnimatedImage(IEnumerable<string> imageNames, TimeSpan interval)
+    : this()
+        {
+            StartAnimation(imageNames, interval);
+        }
+
+        public void StartAnimation(IEnumerable<string> imageNames, TimeSpan interval)
+        {
+            Storyboard storyboard = new Storyboard();
+            ObjectAnimationUsingKeyFrames animation = new ObjectAnimationUsingKeyFrames();
+            Storyboard.SetTarget(animation, image);
+            Storyboard.SetTargetProperty(animation, "Source");
+
+            TimeSpan currentInteval = TimeSpan.FromMilliseconds(0);
+            foreach (string imageName in imageNames)
+            {
+                ObjectKeyFrame keyFrame = new DiscreteObjectKeyFrame();
+                keyFrame.Value = InvadersHelper.CreateImageFromAssets(imageName);
+                keyFrame.KeyTime = currentInteval;
+                animation.KeyFrames.Add(keyFrame);
+                currentInteval = currentInteval.Add(interval);
+            }
+
+            storyboard.RepeatBehavior = RepeatBehavior.Forever;
+            storyboard.AutoReverse = true;
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
+        }
+
         public void InvaderShot()
         {
             invaderShotStoryboard.Begin();
---
+++ InvadersUWP_MVVM/View/InvadersHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace InvadersUWP_MVVM.View
+{
+    class InvadersHelper
+    {
+        public static void StarFactory(int random)
+        {
+            
+        }
+
+        private Color RandomStarColor()
+        {
+            Random rnd = new Random();
+            Byte[] b = new Byte[4];
+            rnd.NextBytes(b);
+            Color color = Color.FromArgb(b[0], b[1], b[2], b[3]);
+            return color;
+        }
+
+        internal static BitmapImage CreateImageFromAssets(string imageFilename)
+        {
+            return new BitmapImage(new Uri("ms-appx:///Assets/" + imageFilename));
+        }
+    }
+}
