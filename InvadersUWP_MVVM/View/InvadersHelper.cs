85ebe20dd13f33979dffdf2d5e232b770debb548
Damian Matusek <shmatuza@hotmail.com>
2017-05-10 21:16:10 +0200

    working on Controls Factory methods

--- InvadersUWP_MVVM/View/InvadersHelper.cs
+++ InvadersUWP_MVVM/View/InvadersHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InvadersUWP_MVVM.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,37 +16,114 @@ namespace InvadersUWP_MVVM.View
 {
     class InvadersHelper
     {
-        private readonly Random _random = new Random();
-        public static FrameworkElement StarFactory(Point point, double scale) //TODO
+        private static readonly Random _random = new Random();
+
+        internal static IEnumerable<string> CreateImageList(Enums.InvaderType invaderType)
+        {
+            string filename;
+            switch(invaderType)
+            {
+                case Enums.InvaderType.Bug:
+                    filename = "bug";
+                    break;
+                case Enums.InvaderType.Satellite:
+                    filename = "satellite";
+                    break;
+                case Enums.InvaderType.Spaceship:
+                    filename = "spaceship";
+                    break;
+                case Enums.InvaderType.Star:
+                default:
+                    filename = "star";
+                    break;
+            }
+            List<string> imageList = new List<string>();
+            for (int i = 1; i < 5; i++)
+                imageList.Add(filename + i + ".png");
+
+            return imageList;
+        }
+
+        internal static FrameworkElement InvaderControlFactory(Invader invader, double scale)
+        {
+            IEnumerable<string> imageList = CreateImageList(invader.InvaderType);
+            AnimatedImage invaderControl = new AnimatedImage(imageList, TimeSpan.FromSeconds(.75));
+            invaderControl.Width = invader.Size.Width * scale;
+            invaderControl.Height = invader.Size.Height * scale;
+            SetCanvasLocation(invaderControl, invader.Location.X * scale, invader.Location.Y * scale);
+
+            return invaderControl;
+        }
+
+        internal static FrameworkElement ShotControlFactory(Shot shot, double scale)
+        {
+            Rectangle shotControl = new Rectangle();
+            shotControl.Fill = new SolidColorBrush(Colors.Yellow);
+            shotControl.Height = Shot.ShotSize.Height * scale;
+            shotControl.Width = Shot.ShotSize.Width * scale;
+            SetCanvasLocation(shotControl, shot.Location.X * scale, shot.Location.Y * scale);
+
+            return shotControl;
+        }
+
+        internal static FrameworkElement StarFactory(Point point, double scale)
         {
             FrameworkElement star;
             switch(_random.Next(3))
             {
+                case 0:
+                    star = new Rectangle();
+                    ((Rectangle)star).Fill = new SolidColorBrush(RandomStarColor());
+                    star.Width = 2;
+                    star.Height = 2;
+                    break;
+                case 1:
+                    star = new Ellipse();
+                    ((Ellipse)star).Fill = new SolidColorBrush(RandomStarColor());
+                    star.Width = 2;
+                    star.Height = 2;
+                    break;
+                default:
+                    star = new Star();
+                    ((Star)star).SetFill(new SolidColorBrush(RandomStarColor()));
+                    break;
+            }
+            SetCanvasLocation(star, point.X * scale, point.Y * scale);
+            Canvas.SetZIndex(star, -1000);
+            return star;
+        }
 
+        internal static FrameworkElement ScanLineFactory(int y, int width, double scale)
+        {
+            FrameworkElement scanLine = new Rectangle();
+            scanLine.Width = width * scale;
+            scanLine.Height = 2;
+            scanLine.Opacity = .5;
+            ((Rectangle)scanLine).Fill = new SolidColorBrush(Colors.White);
+            SetCanvasLocation(scanLine, 0, y * scale);
+            return scanLine;
+        }
+
+        private static Color RandomStarColor()
+        {
+            switch (_random.Next(6))
+            {
+                case 0:
+                    return Colors.White;
+                case 1:
+                    return Colors.LightBlue;
+                case 2:
+                    return Colors.MediumPurple;
+                case 3:
+                    return Colors.PaleVioletRed;
+                case 4:
+                    return Colors.Yellow;
+                default:
+                    return Colors.LightSlateGray;
             }
         }
 
-        private Color RandomStarColor()
-        {
-            Random rnd = new Random();
-            Byte[] b = new Byte[4];
-            rnd.NextBytes(b);
-            Color color = Color.FromArgb(b[0], b[1], b[2], b[3]);
-            return color;
-        }
-
-        public static FrameworkElement ScanLineFactory(int y, int width, double scale)
-        {
-            Rectangle rectangle = new Rectangle();
-            rectangle.Fill = new SolidColorBrush(Colors.White);
-            rectangle.Height = 2;
-            rectangle.Opacity = .1;
-            rectangle.Width = width * scale;
-            SetCanvasLocation(rectangle, 0, y * scale);
-            return rectangle;
-        }
-
-        internal static BitmapImage CreateImageFromAssets(string imageFilename)
+        public static BitmapImage CreateImageFromAssets(string imageFilename)
         {
             return new BitmapImage(new Uri("ms-appx:///Assets/" + imageFilename));
         }
